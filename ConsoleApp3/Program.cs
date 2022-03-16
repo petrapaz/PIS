075f03dc5b7d4a762812c1b62dd65d07713ca9a2
mpijuk <73139482+mpijuk@users.noreply.github.com>
2022-03-16 17:53:32 +0100

    Update Program.cs

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -8,6 +8,7 @@ namespace ConsoleApp3
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("Novo");
+            Console.WriteLine("Promjena2");
         }
     }
 }
